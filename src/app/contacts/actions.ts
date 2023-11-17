0710cee6401ab9d0f780c1f3ddfa6b24106ff100
Andrii Yusyp <serafimsanvol@gmail.com>
2023-11-18 00:21:51 +0200

    Fix env variable name

--- src/app/contacts/actions.ts
+++ src/app/contacts/actions.ts
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
